bcf225751c4cbe39995eb5161a8ea955d0b0ee03
zakariacheknane <cheknanezakaria@gmail.com>
2024-10-03 22:47:21 +0100

    update navbar for user

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,15 @@
 import React, { useEffect, useState } from "react";
 import clockIcon from "../assets/icons/clock.svg";
 import calendarIcon from "../assets/icons/calendar.svg";
+import bellIcon from "../assets/icons/bell.svg";   
+import signalIcon from "../assets/icons/signal.svg"; 
+import menuIcon from "../assets/icons/menu.svg"; // Hamburger menu icon
+import { useSelector } from "react-redux";
+import { RootState } from "../redux/store";
 
 const Navbar: React.FC = () => {
   const [currentTime, setCurrentTime] = useState<Date>(new Date());
+  const user = useSelector((state: RootState) => state.auth.user);
 
   useEffect(() => {
     const timer = setInterval(() => {
@@ -28,22 +34,39 @@ const Navbar: React.FC = () => {
   const formattedTime = currentTime.toLocaleTimeString("fr-FR", timeOptions);
 
   return (
-    <header className="relative w-full bg-custom-gradient shadow-md p-4 flex items-center h-24 justify-center rounded-[16px]">
-      <div className="flex items-center gap-2">
-        <img src={calendarIcon} alt="Calendar Icon"/>
-        <span className="text-[#50799E] text-[24px] leading-[18.56px] font-inter font-normal">
-          {formattedDate}
-        </span>
+    <header
+      className={`relative w-full bg-custom-gradient shadow-md p-4 flex items-center h-20 rounded-[16px] ${
+        user ? "justify-between" : "justify-center"
+      }`}
+    >
+      {user && (
+        <div className="flex items-center gap-2">
+          <img src={menuIcon} alt="Menu Icon" className="w-14 h-14 cursor-pointer" />
+        </div>
+      )}
+
+      <div className="flex justify-center items-center gap-10">
+        <div className="flex items-center gap-2">
+          <img src={calendarIcon} alt="Calendar Icon" />
+          <span className="text-steel-blue text-[24px] leading-[18.56px] font-inter font-normal">
+            {formattedDate}
+          </span>
+        </div>
+
+        <div className="flex items-center gap-2">
+          <img src={clockIcon} alt="Clock Icon" />
+          <span className="text-steel-blue text-[24px] leading-[18.56px] font-inter font-normal">
+            {formattedTime}
+          </span>
+        </div>
       </div>
 
-      <div className="mx-6" />
-
-      <div className="flex items-center gap-2">
-        <img src={clockIcon} alt="Clock Icon"  />
-        <span className="text-[#50799E] text-[24px] leading-[18.56px] font-inter font-normal">
-          {formattedTime}
-        </span>
-      </div>
+      {user && (
+        <div className="flex items-center gap-4">
+          <img src={signalIcon} alt="Signal Icon" className="w-6 h-6" />
+          <img src={bellIcon} alt="Bell Icon" className="w-6 h-6" />
+        </div>
+      )}
     </header>
   );
 };
